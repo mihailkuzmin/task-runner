51863573387d8dc1cf22c80a46ac863f93ff1939
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-03-01 20:36:32 +0300

    Handling exceptions in POST '/process' route

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,18 @@ const Runners = require('./runners')
 const { RunnerService } = require('./services')
 
 router.post('/process', async (req, res) => {
-  const { language, programText } = req.body
-  const runner = new RunnerService(Runners, language)
+  try {
+    const { language, programText } = req.body
+    const runner = new RunnerService(Runners, language)
 
-  const result = await runner.runTask(programText)
+    const result = await runner.runTask(programText)
 
-  res.send(result)
+    res.send(result)
+  } catch (e) {
+    res.status(500).send({
+      error: 'Internal server error',
+    })
+  }
 })
 
 module.exports = router
