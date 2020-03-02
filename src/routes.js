5c22d65b67fb2a9331789409bd0862c35548022a
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-03-02 20:30:09 +0300

    Added POST '/processAll' for process many tasks

--- src/routes.js
+++ src/routes.js
@@ -2,17 +2,39 @@
 
 const router = require('express').Router()
 const Runners = require('./runners')
-const { RunnerService } = require('./services')
+const { RunnerService, TestCheckerService } = require('./services')
 
 router.post('/process', async (req, res) => {
   try {
-    const { language, programText } = req.body
+    const { language, programText, taskId } = req.body
     const runner = new RunnerService(Runners, language)
+    const checker = new TestCheckerService()
 
     const result = await runner.runTask(programText)
+    const testResult = await checker.runTest(result.output, taskId)
+
+    res.send({ ...result, testResult })
+  } catch (e) {
+    res.status(500).send({
+      error: 'Internal server error',
+    })
+  }
+})
+
+router.post('/processAll', async (req, res) => {
+  try {
+    const { programs } = req.body
+    
+    const tasks = programs.map(({language, programText}) => {
+      const runner = new RunnerService(Runners, language)
+      return runner.runTask(programText)
+    })
+
+    const result = await Promise.all(tasks)
 
     res.send(result)
   } catch (e) {
+    console.log(e)
     res.status(500).send({
       error: 'Internal server error',
     })
