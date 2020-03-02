66cb158cbb475e23be059c354a59a610fb37c71d
mihailkuzmin <0mihailkuzmin@gmail.com>
2020-03-02 20:27:11 +0300

    Created service for running tests

---
+++ src/services/TestCheckerService.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const path = require('path')
+
+class TestCheckerService {
+  async runTest(output, taskId) {
+    const testOutput = await this._getTestOutput(taskId)
+    return {
+      passed: output === testOutput,
+      testOutput
+    }
+  }
+
+  async _getTestOutput(taskId) {
+    const filePath = `${path.resolve(__dirname, `../tests/${taskId}.txt`)}`
+
+    const output = await fs.promises.readFile(filePath, 'utf-8')
+    return output
+  }
+}
+
+module.exports = TestCheckerService
--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 /** @format */
 
 const RunnerService = require('./RunnerService')
+const TestCheckerService = require('./TestCheckerService')
 
 module.exports = {
   RunnerService,
+  TestCheckerService
 }
